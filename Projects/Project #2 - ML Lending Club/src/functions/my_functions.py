907476c86a9dcd3b63eedff03e790341562879c6
David Mahler <dmahler26@gmail.com>
2017-11-27 10:39:45 -0500

    WIP Checkpoint

--- Projects/Project #2 - ML Lending Club/src/functions/my_functions.py
+++ Projects/Project #2 - ML Lending Club/src/functions/my_functions.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 import time
+import re
 
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
@@ -14,6 +15,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_sc
 from sklearn.metrics.scorer import make_scorer
 
 from sklearn.metrics import confusion_matrix, f1_score, recall_score, precision_score, precision_recall_curve, fbeta_score
+from sklearn.feature_selection import SelectFromModel, SelectKBest, SelectPercentile, RFECV, f_classif
 
 ### PIPELINE FUNCTIONS ###
 
@@ -103,6 +105,9 @@ class CustomNumAttributes(BaseEstimator, TransformerMixin):
         X['cr_line_td_log'] = X['earliest_cr_line_td'].apply(lambda x: np.log10(x) if x >= 1 else 0)
         self.custom_attr_names.append(['earliest_cr_line_td', 'cr_line_td_log'])
         ###
+        X['emp_length_val'] = data['emp_length'].apply(emp_length_val)
+        self.custom_attr_names.append(['emp_length_val'])
+        ###
         ref_dict = self.ref_dict
         if ref_dict is not None:
             ###
@@ -155,6 +160,9 @@ class CustomBinAttributes(BaseEstimator, TransformerMixin):
         X['verified'] = (X['verification_status'] != 'Not Verified').astype(int)
         self.custom_attr_names.append('verified')
         ###
+        X['term_bin'] = X['term'].str.contains('60').astype(int)
+        self.custom_attr_names.append('term_bin')
+        ###
         return X
     def get_feature_names(self):
         return self.feature_names
@@ -177,6 +185,18 @@ def reference_stats(reference_data):
     d['annual_inc_q10'] = data['annual_inc'].quantile(np.arange(0.1, 1.1, 0.1))
     d['funded_amnt_q10'] = data['funded_amnt'].quantile(np.arange(0.1, 1.1, 0.1))
     return d
+
+
+def emp_length_val(emp_length):
+    s = re.search('[0-9]{1,2}', emp_length)
+    if s:
+        if emp_length[0] == '<':
+            val = 0
+        else:
+            val = int(s.group(0))
+    else:
+        val = -1
+    return val
 
 ###
 
@@ -216,6 +236,10 @@ def classifier_summary(y_actual, y_pred, print_results=True):
    
     return df_cmat, df_scores
 
+def binary_specificity(y_actual, y_pred):
+    cmat = confusion_matrix(y_actual, y_pred)
+    spec = cmat[0,0] / cmat[0,:].sum()
+    return spec
 
 def gs_score_summary(gs):
     '''
@@ -237,7 +261,7 @@ def print_cvs(cvs, scoring='CV'):
     '''
     Prints mean score and standard deviation for a cross_val_score
     '''
-    print('Mean {} score = {:.3f} (+\- {:.3f})'.format(scoring, cvs.mean(), cvs.std()))
+    print('Mean {} score = {:.3f} (+/- {:.3f})'.format(scoring, cvs.mean(), cvs.std()))
     
     
 def plot_learning_curve(estimator, X, y, scoring=None, train_sizes=[0.1, 0.325, 0.55 , 0.775, 1.], cv=None, random_state=None, n_jobs=1, figsize=(10,5), xlim=None, ylim=None, title=None, subplots=False):
@@ -256,7 +280,11 @@ def plot_learning_curve(estimator, X, y, scoring=None, train_sizes=[0.1, 0.325, 
           
     for i, (score, ax) in enumerate(zip(scoring, axs)):
         try:
-            lc = learning_curve(estimator=estimator, X=X, y=y, train_sizes=train_sizes, scoring=score, cv=cv, 
+            if re.search('^f[1-9]', score):
+                scorer = make_scorer(fbeta_score, beta=int(score[1]))
+            else:
+                scorer = score
+            lc = learning_curve(estimator=estimator, X=X, y=y, train_sizes=train_sizes, scoring=scorer, cv=cv, 
                                 random_state=random_state, n_jobs=n_jobs)
             ax.plot(lc[0], np.mean(lc[1], axis=1), label='Train ({})'.format(score.title()), ls='--', alpha=0.5, color=colors[i], lw=2)
             ax.plot(lc[0], np.mean(lc[2], axis=1), label='Validate ({})'.format(score.title()), color=colors[i], lw=2)
@@ -281,7 +309,7 @@ def plot_learning_curve(estimator, X, y, scoring=None, train_sizes=[0.1, 0.325, 
     plt.show()   
 
     
-def plot_pr_curve(model, X, y, label=None, f_score=False, beta=1, cv=3, n_jobs=1):
+def plot_pr_curve(model, X, y, label=None, f_score=False, beta=1, cv=3, n_jobs=1, xlim=None, ylim=None):
     '''
     Plots both PR vs. decision thresholds and PR curve for a given estimator and dataset X, y.
     '''
@@ -307,24 +335,32 @@ def plot_pr_curve(model, X, y, label=None, f_score=False, beta=1, cv=3, n_jobs=1
     axs[0].plot(thresholds, recalls[:-1], 'g-', label='Recall')
     if f_score:
         f_scores = (1 + beta**2) * (precisions * recalls) / ((beta**2 * precisions) + recalls)
-        best_index = np.argmax(f_scores[:-1])
+        best_index = np.nanargmax(f_scores[:-1])
         axs[0].plot(thresholds, f_scores[:-1], 'k--', label='F'+str(beta))
         axs[0].plot([thresholds[best_index], ] * 2, [y_min, max(f_scores[best_index], recalls[best_index], precisions[best_index])],
                     linestyle='-.', color='k', lw=1)
         axs[0].plot(thresholds[best_index], y_min, marker='x', color='k', markeredgewidth=2, ms=6)
         axs[0].plot(thresholds[best_index], f_scores[best_index], marker='x', color='k', markeredgewidth=2, ms=6)
-        axs[0].annotate("%0.2f" % f_scores[best_index], (thresholds[best_index], f_scores[best_index] + 0.01))
-        axs[0].annotate("%0.2f" % thresholds[best_index], (thresholds[best_index], y_min + 0.015))
+        axs[0].annotate("%0.3f" % f_scores[best_index], (thresholds[best_index], f_scores[best_index] + 0.02))
+        axs[0].annotate("%0.3f" % thresholds[best_index], (thresholds[best_index], y_min + 0.02))
         
         axs[0].plot(thresholds[best_index], recalls[best_index], color='g', marker='x', markeredgewidth=2, ms=6)
-        axs[0].annotate("%0.2f" % recalls[best_index], (thresholds[best_index], recalls[best_index] + 0.015))
+        axs[0].annotate("%0.3f" % recalls[best_index], (thresholds[best_index], recalls[best_index] + 0.02))
         
         axs[0].plot(thresholds[best_index], precisions[best_index], color='b', marker='x', markeredgewidth=2, ms=6)
-        axs[0].annotate("%0.2f" % precisions[best_index], (thresholds[best_index], precisions[best_index] + 0.015))
-    x_min = max(thresholds[(len(recalls) - np.argmax(recalls[::-1][:-1]) -1)]*1.1, min(thresholds))
-    x_max = min(thresholds[np.argmax(precisions[:-1])]*1.1, max(thresholds))
-    axs[0].set_xlim(x_min, x_max)
-    axs[0].set_ylim(y_min, y_max)
+        axs[0].annotate("%0.3f" % precisions[best_index], (thresholds[best_index], precisions[best_index] + 0.02))
+    
+    if xlim:
+        axs[0].set_xlim(xlim)
+    else:
+        x_min = max(thresholds[(len(recalls) - np.argmax(recalls[::-1][:-1]) -1)]*1.1, min(thresholds))
+        x_max = min(thresholds[np.argmax(precisions[:-1])]*1.1, max(thresholds))
+        axs[0].set_xlim(x_min, x_max)
+    if ylim:
+        axs[0].set_ylim(ylim)
+    else:    
+        axs[0].set_ylim(y_min, y_max)
+        
     axs[0].set_xlabel('Decision Threshold')
     axs[0].set_ylabel('Precision / Recall')
     axs[0].legend()
@@ -361,8 +397,6 @@ def plot_gs_param(gs, set_xscale=False, ylim=(0,1), val_label=True, figsize=(12,
     if set_xscale:
         ax.set_xscale(set_xscale)
     
-    ax.set_ylim(ylim)
-    
     results = gs.cv_results_
 
     # Get the regular numpy array from the MaskedArray
@@ -392,12 +426,195 @@ def plot_gs_param(gs, set_xscale=False, ylim=(0,1), val_label=True, figsize=(12,
         ax.annotate("%0.2f" % best_score,
                     (X_axis[best_index], best_score + 0.005))
     
+    ax.set_ylim(ylim)
     plt.legend(bbox_to_anchor=(1,1))
     plt.grid('off')
     plt.show()
 ###
+def plot_RFE(estimator, X, y, step=1, cv=3, f_score=False, beta=1, n_jobs=1, figsize=(12,8)):
+    scoring = {'precision': 'precision',
+               'recall': 'recall'}
+    f2_score = make_scorer(fbeta_score, beta=2)
+    RFE = {}
+    plt.clf()
+    plt.figure(figsize=figsize)
+    ax = plt.gca()
+    for scorer in scoring:
+        RFE[scorer] = RFECV(estimator=estimator, step=step, cv=cv, scoring=scoring[scorer], n_jobs=n_jobs)
+        RFE[scorer].fit(X, y)
+        ax.plot(range(1, len(RFE[scorer].grid_scores_) + 1), RFE[scorer].grid_scores_, label=scorer)
+    
+    min_score = min(min(RFE['recall'].grid_scores_), min(RFE['precision'].grid_scores_))
+    max_score = max(max(RFE['recall'].grid_scores_), max(RFE['precision'].grid_scores_))
+    
+    ylim = ax.get_ylim()
+    
+    if f_score:
+        f_scores = (1 + beta**2) * (RFE['recall'].grid_scores_ * RFE['precision'].grid_scores_) / ((beta**2 * RFE['precision'].grid_scores_) + RFE['recall'].grid_scores_)
+        ax.plot(range(1, len(f_scores) + 1), f_scores, label='F'+str(beta), color='k', linestyle='--')
+        best_score = np.nanargmax(f_scores)
+        ax.plot([best_score+1,]*2, [ylim[0], f_scores[best_score]], linestyle='-.', color='k', lw=1, marker='x', markeredgewidth=2, ms=6)
+        ax.annotate("%0.2f" % (f_scores[best_score]), (best_score+1, f_scores[best_score] + 0.01))
+        ax.annotate("%0.0f" % (best_score+1), (best_score+1, ylim[0] + 0.01))
+        RFE['F'+str(beta)] = f_scores
+    
+    plt.ylim(ylim)
+    plt.xlabel('N Features Selected')
+    plt.ylabel('Accuracy Score (CV={})'.format(cv))
+    plt.title('Recursive Feature Elimination Scores')
+    plt.legend(bbox_to_anchor=(1,1))    
+    plt.show()
+    return RFE
+###
+def plot_pca_var(X, p=0.9):
+
+    pca = PCA()
+    pca.fit(X)
+
+    plt.figure(figsize=(10,5))
+
+    n = len(pca.explained_variance_ratio_)
+    exp_var = pca.explained_variance_ratio_
+    exp_var_cumsum = np.cumsum(exp_var)
+
+    x = next(i for i, n in enumerate(exp_var_cumsum) if n >= p) + 1
+
+    plt.bar(np.arange(1, n+1, 1), exp_var, align='center', label='Explained Variance Ratio')
+    plt.step(np.arange(1, n+1, 1), exp_var_cumsum, where='mid', label='Cumulative Explained Variance Ratio')
+    
+    plt.hlines(p, 0, n, label='{}% Explained Variance Ratio'.format(p*100), lw=1, color='r', linestyle='dashed')
+    plt.plot([x, x], [0, p], linestyle='--',color='k', markeredgewidth=3, ms=8, lw=1)
+    plt.plot([x], [p], marker='x', color='k', markeredgewidth=2, ms=8, lw=.5)
+    plt.annotate(x, (x+.5, p-0.05))
+    
+    plt.xlabel('Principal Components')
+    plt.ylabel('Explained Variance Ratio')
+    plt.legend(bbox_to_anchor=(1,1))
+    plt.xlim(0, n)
+    plt.title('Principal Component Analysis')
+        
+    plt.show()
+    return pca
+###
+def plot_sfm(estimator, X, y, p_range=np.arange(0, 3.1, 0.5), scoring='accuracy', cv=3, p_ref='median', prefit=False, figsize=(12,8), verbose=False):
+    
+    score_mean = []
+    score_std = []
+    thresholds = []
+
+    for i, p in enumerate(p_range):
+        
+        t = str(p)+'*'+p_ref
+        sfm = SelectFromModel(estimator=estimator, prefit=prefit, threshold=t)
+        if verbose:
+            print("{}/{}: threshold = {:15s}\r".format(i+1, len(p_range), t), end='')
+        if prefit:
+            X_sfm = sfm.transform(X)
+        else:
+            X_sfm = sfm.fit_transform(X, y)
+             
+        f_search = re.search('[Ff]([0-9])', scoring)
+        if f_search:
+            scorer=make_scorer(fbeta_score, beta=float(f_search.group(1)))
+        else:
+            scorer=scoring
+
+        cvs = cross_val_score(estimator=estimator, X=X_sfm, y=y, scoring=scorer, cv=3)
+        score_mean.append(cvs.mean())
+        score_std.append(cvs.std())
+        thresholds.append(t)
+
+    if verbose:
+        print('{:100s}'.format('Complete'))
+    
+    score_mean = np.array(score_mean)
+    score_std = np.array(score_std)
+    
+    plt.clf()
+    plt.figure(figsize=figsize)
+    
+    p = plt.plot(p_range, score_mean)
+    plt.fill_between(p_range, score_mean - score_std, score_mean + score_std, color=p[0].get_color(), alpha=0.15)
+    
+    ylim=plt.ylim()
+    
+    best_index = np.nanargmax(score_mean)
+    best_p = p_range[best_index]
+    best_score = score_mean[best_index]
+    plt.plot([best_p,]*2, [ylim[0], best_score], ls='--', color='k', marker='x', markeredgewidth=3, ms=8, lw=1)
+    plt.annotate('{:.3f}'.format(best_score), (best_p+(plt.xlim()[1]-plt.xlim()[0])*.01, best_score+(ylim[1]-ylim[0])*.01))
+    plt.annotate('{:.1f}'.format(best_p), (best_p+(plt.xlim()[1]-plt.xlim()[0])*.01, ylim[0]+(ylim[1]-ylim[0])*.01))
+    
+    plt.ylim(ylim)
+    plt.xlabel('x*'+p_ref)
+    plt.ylabel('{} Score'.format(scoring.title()))
+    plt.title('SelectFromModel Scores ({}) vs. Thresholds'.format(scoring.title()))
+    plt.show()
+    
+    results = {'thresholds': thresholds,
+               'p': np.array(p_range),
+               'score_mean': score_mean,
+               'score_std': score_std}
+    
+    return results
+###
+def plot_gs_param_dual(gs, scoring, swap_axes=False, cmap='GnBu', power_scale=1, cbar=True, figsize=(12,9)):
+    '''
+    Plot scores with varying grid search param values (only compatiable with single param grid searches)
+    '''
+    if len(gs.param_grid) != 2:
+        print('Error: grid search contains less/more than two parameters. Function only compatible with dual parameter grid searches.')
+        return None
+    
+    params = list(gs.param_grid.keys())
+    if swap_axes:
+        param_B = params[0]
+        param_A = params[1]
+    else:
+        param_A = params[0]
+        param_B = params[1]
+    
+    plt.figure(figsize=figsize)
+    plt.title("GridSearchCV {} Scores for Parameters ({}, {})".format(scoring, param_A, param_B), fontsize=16)
+
+    plt.xlabel(param_A)
+    plt.ylabel(param_B)
+
+    ax = plt.axes()
+    
+    results = gs.cv_results_
+
+    # Get the regular numpy array from the MaskedArray
+    X_axis = np.array(results['param_' + param_A].data)
+    Y_axis = np.array(results['param_' + param_B].data)
+        
+    score_data = results['mean_test_%s' % (scoring)]
+    
+    df = pd.DataFrame(np.vstack((X_axis, Y_axis, score_data)).T).rename(columns={0: param_A, 1: param_B, 2: 'score'})
+    
+    sns.heatmap(df.pivot(param_A, param_B, 'score').astype(float)**power_scale, cmap=cmap, cbar=cbar)
+
+    plt.show()
+###
 
 ### MISC HELPER FUNCTIONS ###
+###
+def rank_array(array, absolute=False, ascending=True):
+    if absolute:
+        temp = abs(array)
+    else:
+        temp = array
+        
+    if ascending:
+        temp=temp.argsort()
+    else:
+        temp=temp.argsort()[::-1]
+    
+    ranks = np.empty(len(array), int)
+    ranks[temp] = np.arange(len(array))
+    
+    return ranks
+###
 
 def run_time(reset=False, print_time=True, return_time=False, pretty=True):
     '''
@@ -422,3 +639,4 @@ def run_time(reset=False, print_time=True, return_time=False, pretty=True):
             print(display)
         if return_time:
             return td
+###
